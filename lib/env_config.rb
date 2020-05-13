6136b77d0ef53ba5c50d514a7b6dc5aca361aaaf
Paul Sadauskas <psadauskas@gmail.com>
2020-05-13 12:08:16 -0600

    Generate new gem with 'bundle gem'

---
+++ lib/env_config.rb
@@ -0,0 +1,6 @@
+require "env_config/version"
+
+module EnvConfig
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/env_config_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe EnvConfig do
+  it "has a version number" do
+    expect(EnvConfig::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

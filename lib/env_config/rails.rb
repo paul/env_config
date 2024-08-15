33f95cbc5401763e3c0f674cf3744a6af8f1dc49
Paul Sadauskas <psadauskas@gmail.com>
2024-08-15 13:23:42 -0600

    Update rails extension

--- lib/env_config/rails.rb
+++ lib/env_config/rails.rb
@@ -4,15 +4,25 @@ require "active_support/string_inquirer"
 
 class EnvConfig
   module Rails
-    def initialize(env = ENV, app_env: Rails.env)
+    extend Forwardable
+
+    attr_reader :app_env
+
+    def initialize(env = ENV, app_env: ::Rails.env)
       super
-      @app_env = app_Env
+      @app_env = app_env
     end
 
+    # Works like #fetch, but converts "truthy" or "falsy" values to a Boolean
+    # "t", "true", "1" => true
+    # "f", "false", "0", "" => false
     def fetch?(name, *, &)
       ActiveModel::Type::Boolean.new.cast(fetch(name, *, &))
     end
 
-    delegate %i[development? test? production?] => :app_env
+    delegate %i[local?] => :@app_env
+
+    # When using EnvConfig.fetch!, anything not dev or test is considered "production".
+    def production? = !local?
   end
 end
